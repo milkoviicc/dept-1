bc9984175b5bc950091cb04dc72b25d628442fff
Marko Milković <mmilkovic2006@gmail.com>
2025-05-30 16:07:09 +0200

    Added useEffect for navigation in home page

--- src/(pages)/home.tsx
+++ src/(pages)/home.tsx
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const token = localStorage.getItem('token');
 
   const navigate = useNavigate();
-  if(!token) {
-    navigate('/register');
-  }
+
+  useEffect(() => {
+    if(!token) {
+      navigate('/register');
+    }
+
+  }, [token, navigate]);
   return (
 
     <div className='w-full flex flex-col justify-center items-center'>
